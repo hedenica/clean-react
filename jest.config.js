4a5ca5a4e0fb724cebcb0c621f97d50109ad5b76
hedenicapedrosa <hedenica@hotmail.com>
2021-02-15 18:37:35 -0300

    chore: add jest config

--- jest.config.js
+++ jest.config.js
@@ -2,15 +2,15 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
-    '!<rootDir>/src/main/**/*',
-    '!<rootDir>/src/**/index.ts',
-    '!**/*.d.ts'
+    // '!<rootDir>/src/main/**/*',
+    // '!<rootDir>/src/**/index.ts',
+    // '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
-  setupFilesAfterEnv: ['<rootDir>/src/main/config/jest-setup.ts'],
+  // setupFilesAfterEnv: ['<rootDir>/src/main/config/jest-setup.ts'],
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/',
-    '<rootDir>/src/main/test/cypress'
+    // '<rootDir>/src/main/test/cypress'
   ],
   testEnvironment: 'jsdom',
   transform: {
